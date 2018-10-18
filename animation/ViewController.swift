8b85e13ea4537bb667f127ece34f400d07ab672b
Kaila Prochaska <kailaprochaska@wireless-10-148-161-44.public.utexas.edu>
2018-10-18 18:06:14 -0500

    Initial Commit

---
+++ animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animation
+//
+//  Created by Kaila Prochaska on 10/18/18.
+//  Copyright © 2018 Kaila Prochaska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
